303ad5086506d27a6fbe91f466c2d6710ba1aa1f
NazarChumbey <nazzichumbey@gmail.com>
2019-03-22 14:23:50 +0200

    Adding POJOs

---
+++ src/main/java/nazar/chumbey/com/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package nazar.chumbey.com.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/nazar/chumbey/com/springpetclinic/model/PetType.java
@@ -0,0 +1,18 @@
+package nazar.chumbey.com.springpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
+
+
